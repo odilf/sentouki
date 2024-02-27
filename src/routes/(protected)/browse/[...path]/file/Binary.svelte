7b85fb761dbe902444205ea1187a772487b916b5
Odilf <odysseas.maheras@gmail.com>
2024-02-27 23:10:07 +0100

    Added simple binary data

---
+++ src/routes/(protected)/browse/[...path]/file/Binary.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button'
+
+    export let serverPath: string
+
+    async function getStream(serverPath: string) {
+        const response = await fetch(serverPath)
+        const blob = await response.blob()
+        return await blob.arrayBuffer()
+    }
+
+    // let byteStream: ReadableStream<Uint8Array> | null = null
+    let bytes: ArrayBuffer | null = null
+
+</script>
+
+Binary data. UOH File Browser doesn't yet support interesting ways to represent binary data.
