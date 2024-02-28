fe344c0032e3f0a3a6513a1670ab89345d69c3a2
Odilf <odysseas.maheras@gmail.com>
2024-02-28 23:05:14 +0100

    the godforsaken lack of spaces after //

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,9 @@
 const config = {
     plugins: {
         'tailwindcss/nesting': {},
-        //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+        // Some plugins, like tailwindcss/nesting, need to run before Tailwind,
         tailwindcss: {},
-        //But others, like autoprefixer, need to run after,
+        // But others, like autoprefixer, need to run after,
         autoprefixer: {},
     },
 }
