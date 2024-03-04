1b17db7adc4d6f2e9aa25233ef6df4a03b8f4bac
Odilf <odysseas.maheras@gmail.com>
2024-03-04 15:30:29 +0100

    favicon

--- uoh-file-browser/vite.config.ts
+++ uoh-file-browser/vite.config.ts
@@ -2,9 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { vite as vidstack } from 'vidstack/plugins'
 import { defineConfig } from 'vite'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon2';
 
 export default defineConfig({
     plugins: [
+        ViteFaviconsPlugin("./static/favicon.svg"),
         vidstack({ include: /player\// }),
         topLevelAwait({}),
         sveltekit(),
